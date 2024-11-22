57a2d6a99d5b2002788ad611a17df2800a6abc3a
Michael Jiang <mjiang@lcs.on.ca>
2024-11-22 08:38:54 -0500

    Initial Commit

---
+++ FavouriteThings/FavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteThingsApp.swift
+//  FavouriteThings
+//
+//  Created by Michael on 2024/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
